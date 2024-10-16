91fdba232a02b9f762ede909495f10d843577174
Madiyar <madiarduysenbay@gmail.com>
2024-10-17 03:24:50 +0500

    commit

--- fast_read_app/views.py
+++ fast_read_app/views.py
@@ -8,14 +8,14 @@ from .read_request_handler import read_request_handler
 
 
 class HomePageView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'fast-read.html'
 
     def post(self, request, *args, **kwargs):
         try:
             data = json.loads(request.body)
             test_id = data.get('id')
             text = data.get('text')
-            tel_number = data.get('tel_number')
+            tel_number = data.get('whatsapp_number')
             time_spent = data.get('time')
             answers = data.get('answers')
             
